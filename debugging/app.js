839e0d83cceacab62cfd2af496179b9e9ce7ecfd
hashimmalik59 <hm3630450@gmail.com>
2026-08-16 21:52:44 +0500

    Syntax debugging

---
+++ debugging/app.js
@@ -0,0 +1,11 @@
+// const username = "Hashim";
+// const age = 23;
+
+// if (age >= 18) {
+//   console.log("Welcome " + userName);
+// }
+
+// 1. ReferenceError: userName is not defined
+// 2. kyun k username ki jagah userName mein n capital hai
+// 3. 6th
+// 4. N => n
