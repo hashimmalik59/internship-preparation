8e096224acff285bf63fd7c1289e31061f1ef83c
hashimmalik59 <hm3630450@gmail.com>
2026-08-16 21:59:03 +0500

    Logicall error debug

--- debugging/app.js
+++ debugging/app.js
@@ -10,15 +10,32 @@
 // 3. 6th
 // 4. N => n
 
-const age = 17;
-const hasID = true;
+// const age = 17;
+// const hasID = true;
 
-if (age >= 18 || hasID) {
-  console.log("You can enter");
-} else {
-  console.log("You cannot enter");
-}
+// if (age >= 18 || hasID) {
+//   console.log("You can enter");
+// } else {
+//   console.log("You cannot enter");
+// }
 // 1. || ki jagah && hona chahiye
 // 2. only logic error
 // 3. sirf aik condition check kar raha hai
 // 4. (age >= 18 && hasID)
+
+// const users = [
+//   { name: "Ali", age: 20 },
+//   { name: "Hashim", age: 25 },
+//   { name: "Ahmed", age: 17 },
+// ];
+
+// const adultUsers = users.filter((user) => {
+//   user.age >= 18;
+// });
+
+// console.log(adultUsers);
+
+// 1. Logical error hai
+// 2. return nahi lagaya hua hai kyun k brackets hain
+// 3. { name: "Ali", age: 20 }, { name: "Hashim", age: 25 },
+// 4. new array
