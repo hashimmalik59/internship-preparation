834eeb705dee2d88a33141b9a7f5e8264fbe4fbc
hashimmalik59 <hm3630450@gmail.com>
2026-08-16 21:54:21 +0500

    Logical debugging

--- debugging/app.js
+++ debugging/app.js
@@ -9,3 +9,16 @@
 // 2. kyun k username ki jagah userName mein n capital hai
 // 3. 6th
 // 4. N => n
+
+const age = 17;
+const hasID = true;
+
+if (age >= 18 || hasID) {
+  console.log("You can enter");
+} else {
+  console.log("You cannot enter");
+}
+// 1. || ki jagah && hona chahiye
+// 2. only logic error
+// 3. sirf aik condition check kar raha hai
+// 4. (age >= 18 && hasID)
