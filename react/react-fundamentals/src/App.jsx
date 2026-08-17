822d12f9f00d8a06b48b6fdf34540f1e7b43a6eb
hashimmalik59 <hm3630450@gmail.com>
2026-08-17 16:04:51 +0500

    challenge 1 for useState, mini count app

---
+++ react/react-fundamentals/src/App.jsx
@@ -0,0 +1,49 @@
+// import { useState } from "react";
+
+import Challenge1 from "./challenges/01-challenge";
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   function increaseCount() {
+//     setCount((prev) => prev + 1);
+//   }
+//   return (
+//     <div>
+//       <button onClick={increaseCount}>Count</button>
+//       {count}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// function App() {
+// return (
+//     <div>
+//       <User name="Hashim" />
+//       <User name="Qazi" />
+//       <User name="Shafqat" />
+//       <User name="Hakeem" />
+//     </div>
+//   );
+// }
+// export default App;
+
+// function User(props) {
+//   return (
+//     <div>
+//       <span>Hello {props.name}</span>
+//       <br />
+//     </div>
+//   );
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Challenge1 />
+    </div>
+  );
+};
+
+export default App;
