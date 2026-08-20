24a636b0b21cd6c6f3d75d2fe50b355cf273a94a
hashimmalik59 <hm3630450@gmail.com>
2026-08-20 17:09:12 +0500

    JS before react: destructuring, practice exercise 1

---
+++ js-before-react/01-destructring.js
@@ -0,0 +1,6 @@
+const colors = ["Red", "Green", "Blue", "Yellow"];
+
+// Yahan destructuring ka code khud likh:
+const [firstColor, secondColor] = colors;
+
+console.log(firstColor, secondColor); // Expected output: Red Green
