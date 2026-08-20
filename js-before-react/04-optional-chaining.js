9d34c4c8081219559460bc79369ae4d28aee98c1
hashimmalik59 <hm3630450@gmail.com>
2026-08-20 19:34:12 +0500

    JS before react: optional chaining, for preventing app crash

---
+++ js-before-react/04-optional-chaining.js
@@ -0,0 +1,13 @@
+// const user = {
+//   name: "Ali",
+// };
+
+// console.log(user.address.city);
+
+// console.log(user?.address?.city);
+
+// const profile = {
+//   username: "codermain",
+// };
+
+// console.log(profile?.details?.location);
