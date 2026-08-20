3b234fd4db87acbfe2b11a0b02663514dd96d3f3
hashimmalik59 <hm3630450@gmail.com>
2026-08-20 18:15:29 +0500

    JS before react: spread operator, exercise, combine group A & B members and add and more member at the end

---
+++ js-before-react/02-spread-operator.js
@@ -0,0 +1,19 @@
+// Task ki W Wazahat:
+// Tujhe ek naya array banana hai allMembers ke naam se. Uske andar:
+
+// groupA ki sari values spread karni hain (...groupA).
+
+// groupB ki sari values spread karni hain (...groupB).
+
+// Sath mein ek string "Zain" bhi add karni hai.
+
+// Ab is logic ko use kar ke code khud likh aur yahan bhej.
+
+// const groupA = ["Ali", "Ahmed"];
+// const groupB = ["Bilal", "Usman"];
+
+// const allMembers = [...groupA, ...groupB, "Zain"];
+
+// console.log(groupA);
+// console.log(groupB);
+// console.log(allMembers);
