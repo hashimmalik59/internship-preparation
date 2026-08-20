f94b705c4dbacc3782663378af98fef69a94d6ef
hashimmalik59 <hm3630450@gmail.com>
2026-08-20 17:10:35 +0500

    JS before react: destructuring, practice exercise 2, skipping an element in an array

--- js-before-react/01-destructring.js
+++ js-before-react/01-destructring.js
@@ -1,6 +1,13 @@
+// const colors = ["Red", "Green", "Blue", "Yellow"];
+
+// // Yahan destructuring ka code khud likh:
+// const [firstColor, secondColor] = colors;
+
+// console.log(firstColor, secondColor); // Expected output: Red Green
+
 const colors = ["Red", "Green", "Blue", "Yellow"];
 
-// Yahan destructuring ka code khud likh:
-const [firstColor, secondColor] = colors;
+// Yahan destructuring kar ke Green ko skip kar:
+const [firstColor, , thirdColor] = colors;
 
-console.log(firstColor, secondColor); // Expected output: Red Green
+console.log(firstColor, thirdColor); // Expected output: Red Blue
