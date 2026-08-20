e16a8d56f61100d2f0a2b257b591f85d27893a72
hashimmalik59 <hm3630450@gmail.com>
2026-08-20 11:01:41 +0500

    useEffect hook, practice

--- react/react-fundamentals/src/App.jsx
+++ react/react-fundamentals/src/App.jsx
@@ -1,46 +1,27 @@
-// import { useState } from "react";
-// import LikeButton from "./challenges/LikeButton";
-// import LikeDisplay from "./challenges/LikeDisplay";
-// import ConditionalRendering from "./challenges/ConditionalRendering";
-// import ImageObject from "./challenges/ImageObject";
-
-import { useState } from "react";
-
-// import ListRendering from "./challenges/ListRendering";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  // const [like, setLike] = useState(0);
-  // function handleLike() {
-  //   setLike(like + 1);
-  // }
   const [count, setCount] = useState(0);
-
+  const [like, setLike] = useState(0);
+  useEffect(() => {
+    document.title = `My page ${count}`;
+    console.log("hello");
+  }, [count, like]);
   return (
     <div>
-      {/* <ImageObject />
-      <LikeButton onLike={handleLike} />
-      <LikeDisplay displayLike={like} /> */}
-      {/* <ConditionalRendering /> */}
-      {/* <ListRendering /> */}
+      <h1>Hello React</h1>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-green-600 text-white"
+      >
+        Count: {count}
+      </button>
       <br />
-      <span className={!count && "text-red-600"}>{count}</span>
       <button
-        onClick={() => setCount((prevCount) => prevCount + 1)}
-        className="bg-green-600 text-white border rounded px-2 font-extrabold"
+        onClick={() => setLike(like + 1)}
+        className="bg-green-600 text-white"
       >
-        +
-      </button>
-      <button
-        onClick={() => setCount(0)}
-        className="bg-red-600 text-white border rounded px-2 font-extrabold"
-      >
-        0
-      </button>
-      <button
-        onClick={() => setCount((prevCount) => prevCount - 1)}
-        className="bg-gray-600 text-white border rounded px-2 font-extrabold"
-      >
-        -
+        Like: {like}
       </button>
     </div>
   );
