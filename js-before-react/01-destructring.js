73a906b53157b02e59b339675d40fd0000f85b23
hashimmalik59 <hm3630450@gmail.com>
2026-08-20 17:11:58 +0500

    JS before react: destructuring, practice exercise 2, setting default values

--- js-before-react/01-destructring.js
+++ js-before-react/01-destructring.js
@@ -5,9 +5,16 @@
 
 // console.log(firstColor, secondColor); // Expected output: Red Green
 
-const colors = ["Red", "Green", "Blue", "Yellow"];
+// const colors = ["Red", "Green", "Blue", "Yellow"];
 
-// Yahan destructuring kar ke Green ko skip kar:
-const [firstColor, , thirdColor] = colors;
+// // Yahan destructuring kar ke Green ko skip kar:
+// const [firstColor, , thirdColor] = colors;
 
-console.log(firstColor, thirdColor); // Expected output: Red Blue
+// console.log(firstColor, thirdColor); // Expected output: Red Blue
+
+const scores = [90, 80];
+
+// Yahan destructuring kar ke teesri value ke liye default value set kar:
+const [math = 90, english = 80, science = 50] = scores;
+
+console.log(math, english, science); // Expected output: 90 80 50
