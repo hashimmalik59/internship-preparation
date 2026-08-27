1cbf1d55c8695139fee2e62f370e6fb36137bd38
hashimmalik59 <hm3630450@gmail.com>
2026-08-27 21:19:18 +0500

    Problem solving: 'Counting all positive number from array

--- problems/app.js
+++ problems/app.js
@@ -9,3 +9,17 @@ function sumAllArrNums(arr) {
 // console.log(sumAllArrNums([]));
 // console.log(sumAllArrNums([5]));
 // console.log(sumAllArrNums([-2, 5, -3]));
+
+function positiveLength(arr) {
+  let positiveCount = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) positiveCount++;
+  }
+  return positiveCount;
+}
+// console.log(positiveLength([-3, 5, -1, 8, 0, 2, -7]));
+// console.log(positiveLength([]));
+// console.log(positiveLength([-5, -2, -9]));
+// console.log(positiveLength([1, 2, 3]));
+// console.log(positiveLength([0, 0, 0]));
+// console.log(positiveLength([-1, 0, 5]));
