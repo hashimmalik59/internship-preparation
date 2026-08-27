265e7964f8a2ef1073be62f605e8473cfb9c9757
hashimmalik59 <hm3630450@gmail.com>
2026-08-27 21:05:03 +0500

    Problem solving: 'Sum all array numbers for'

---
+++ problems/app.js
@@ -0,0 +1,11 @@
+function sumAllArrNums(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+// console.log(sumAllArrNums([5, 10, 3, 7, 2]));
+// console.log(sumAllArrNums([]));
+// console.log(sumAllArrNums([5]));
+// console.log(sumAllArrNums([-2, 5, -3]));
