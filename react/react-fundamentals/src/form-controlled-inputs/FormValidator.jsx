3513cfc33266a2c74132a0f85972a3e24841be3b
hashimmalik59 <hm3630450@gmail.com>
2026-09-05 13:59:02 +0500

    Basic form validator

--- react/react-fundamentals/src/form-controlled-inputs/FormValidator.jsx
+++ react/react-fundamentals/src/form-controlled-inputs/FormValidator.jsx
@@ -1,14 +1,42 @@
 import { useState } from "react";
 
 const FormValidator = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+  function handlePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email.length > 5 && password.length > 5) {
+      console.log("Successfullu logged in");
+    }
+  }
+
   return (
     <div>
-      <form className="border flex flex-col justify-center gap-5 m-2 p-2">
-        <input type="email" placeholder="email" className="border w-full p-2" />
+      <form
+        className="border flex flex-col justify-center gap-5 m-2 p-2"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="email"
+          className="border w-full p-2"
+          onChange={handleEmail}
+          value={email}
+        />
         <input
           type="password"
           placeholder="password"
           className="border w-full p-2"
+          onChange={handlePassword}
+          value={password}
         />
         <input
           type="submit"
