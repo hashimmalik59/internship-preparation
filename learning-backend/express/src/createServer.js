094375015142235f202f087b46d07b51ffd74440
hashimmalik59 <hm3630450@gmail.com>
2026-09-19 02:03:19 +0500

    Read, Delete and Update of CRUD REST API task

--- learning-backend/express/src/createServer.js
+++ learning-backend/express/src/createServer.js
@@ -4,14 +4,43 @@ const app = express();
 
 app.use(express.json());
 
-const notes = [];
+const students = [];
 
-app.post("/notes", (req,res) => {
-    notes.push(req.body);
+app.post("/students", (req, res) => {
+  students.push(req.body);
 
-    res.status(201).json({
-        message: "note is created successfully"
-    })
-})
+  res.status(201).json({
+    message: "student data is created successfully",
+  });
+});
+
+app.get("/students", (req, res) => {
+  res.status(200).json({
+    message: "students data is fetched successfully",
+    students: students,
+  });
+});
+
+app.delete("/students/:index", (req, res) => {
+  const index = req.params.index;
+
+  delete students[index];
+
+  res.status(200).json({
+    message: "student data is deleted successfully",
+  });
+});
+
+app.patch("/students/:index", (req, res) => {
+  const index = req.params.index;
+
+  const name = req.body.name;
+
+  students[index].name = name;
+
+  res.status(200).json({
+    message: "student data is updated successfully",
+  });
+});
 
 export default app;
